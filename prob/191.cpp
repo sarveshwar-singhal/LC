5d84c62979f22c77d62678a5ab171dfbda878f0b
mag01 <sarveshwar.kta@gmail.com>
2022-08-27 22:25:34 -0400

    wc308 start

--- prob/191.cpp
+++ prob/191.cpp
@@ -1,6 +1,14 @@
 #include<iostream>
 using namespace std;
 
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        
+    }
+};
+
+
 int main(int argc, char const *argv[])
 {
     return 0;
