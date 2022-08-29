befa74e6be253f85b951b7c5881d9b9169f26f55
mag01 <sarveshwar.kta@gmail.com>
2022-08-28 21:44:12 -0400

    adding optimized solution

--- wc/308_1.cpp
+++ wc/308_1.cpp
@@ -4,7 +4,65 @@ using namespace std;
 
 class Solution {
 public:
+    //try 3: no new vector for prefix sum. used existing nums
     vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
+        sort(nums.begin(), nums.end());
+        vector<int> out(queries.size(), 0);
+        int i;
+        for(i=1;i<nums.size();i++){
+            nums[i] += nums[i-1];
+        }
+        int l, r, mid, key;
+        for(i=0;i<queries.size();i++){
+            key = queries[i];
+            l=0, r=nums.size()-1;
+            while(l<=r){
+                mid = (l+r)/2;
+                if(key < nums[mid]){
+                    r = mid-1;
+                } else if(key > nums[mid]){
+                    l = mid+1;
+                } else{
+                    l = mid+1;
+                    break;
+                }
+            }
+            out[i] = l;
+        }
+        return out;
+    }
+    //try2: used binary search for finding base index
+    vector<int> answerQueries2(vector<int>& nums, vector<int>& queries) {
+        sort(nums.begin(), nums.end());
+        vector<int> psum(nums.size(), 0);
+        vector<int> out(queries.size(), 0);
+        int i;
+        psum[0] = nums[0];
+        for(i=1;i<nums.size();i++){
+            psum[i] += psum[i-1] + nums[i];
+        }
+        int l, r, mid, key;
+        for(i=0;i<queries.size();i++){
+            key = queries[i];
+            l=0, r=psum.size()-1;
+            while(l<=r){
+                mid = (l+r)/2;
+                if(key < psum[mid]){
+                    r = mid-1;
+                } else if(key > psum[mid]){
+                    l = mid+1;
+                } else{
+                    l = mid+1;
+                    break;
+                }
+            }
+            out[i] = l;
+        }
+        return out;
+    }
+
+    //used linear search for finding base index
+    vector<int> answerQueries1(vector<int>& nums, vector<int>& queries) {
         sort(nums.begin(), nums.end());
         // for(auto x: nums){
         //     cout << x << " ";
