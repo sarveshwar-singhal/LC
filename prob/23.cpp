7f2f25347ea71dc25387f6ddd65d3103d97ce4f5
win_mag1 <scpl.singhal@gmail.com>
2023-03-12 04:59:32 -0400

    prob 23 done

--- prob/23.cpp
+++ prob/23.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<map>
 using namespace std;
 
 // Definition for singly-linked list.
@@ -12,30 +13,69 @@ struct ListNode {
 };
 
 class Solution {
-    ListNode *head=NULL, *top=NULL;
+    ListNode* head=NULL, *top=NULL;
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         int i, minIdx=0;
         bool elePresent = true;
-        while(elePresent){
-            // minIdx = 0;
-            for(i=0;i<=lists.size();i++){
+        while(minIdx >= 0){
+            for(i=0;i<lists.size();i++){
                 if(lists[i]==NULL) continue;
                 if(lists[i]->val < lists[minIdx]->val){
                     minIdx = i;
                 }
             }
-            insert(lists[i]);
+            // cout << "value of minIdx " << i << endl;
+            insert(lists[minIdx]);
+            lists[minIdx] = lists[minIdx]->next;
+            minIdx = getFirstAvailIdx(lists);
+        }
+        return head;
+    }
 
+    void insert(ListNode* lists){
+        cout << "inside insert " << endl;
+        if(this->head == NULL){
+            this->head = new ListNode(lists->val);
+            this->top = this->head;
+        } else {
+            this->top->next = new ListNode(lists->val);
+            this->top = this->top->next;
         }
     }
 
-    void insert(ListNode* &lists){
-        if(this->head == NULL){
-            head = new ListNode(lists->val);
-            top = head;
-        } else {
-            top->next = new ListNode(lists->val);
+    int getFirstAvailIdx(vector<ListNode*> &lists){
+        for(int i=0;i<lists.size();i++){
+            if(lists[i] != NULL) return i;
         }
+        return -1;
+    }
+};
+
+
+class Solution1 {
+    ListNode* head=NULL, *top=NULL;
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        map<int, int> freq;
+        for(ListNode* &x: lists){
+            while(x!= NULL){
+                freq[x->val]++;
+                x = x->next;
+            }
+        }
+        int y;
+        for(pair<int, int> x: freq){
+            for(y=0;y<x.second;y++){
+                ListNode *n = new ListNode(x.first);
+                if(top==NULL){
+                    head = top = n;
+                } else {
+                    top->next = n;
+                    top = top->next;
+                }
+            }
+        }
+        return head;
     }
 };
